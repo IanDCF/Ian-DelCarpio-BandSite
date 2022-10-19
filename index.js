c0d1a6bc9ab2269eea864350a8f5693f0e372641
Ian DC <jokerzamba@hotmail.com>
2022-10-20 00:09:55 +0200

    Complete basic form handling + comments rendering functionality

--- index.js
+++ index.js
@@ -5,27 +5,28 @@ const comments = [
   {
     id: uniqueID(),
     name: "Connor Walton",
+    date: "02/17/2021",
     comment:
       "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains",
-    date: "02/17/2021",
   },
   {
     id: uniqueID(),
     name: "Emilie Beach",
+    date: "01/09/2021",
     comment:
       "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-    date: "01/09/2021",
   },
   {
     id: uniqueID(),
-    name: "12/20/2020",
+    name: "Miles Acosta",
+    date: "12/20/2020",
     comment:
       "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-    date: "12/20/2020",
   },
 ];
 
-const renderTask = (taskObj) => {
+// RENDER COMMENTS __________________________________________
+const renderComment = (taskObj) => {
   let forumComments = document.querySelector(".forum__comments");
 
   let forumComment = document.createElement("div");
@@ -73,8 +74,30 @@ const render = () => {
   commentsList.innerHTML = "";
 
   for (let i = 0; i < comments.length; i++) {
-    renderTask(comments[i]);
+    renderComment(comments[i]);
   }
 };
 
 render();
+
+// HANDLE FORM ___________________________________________________________
+let commentButton = document.querySelector(".forum__button");
+
+commentButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  let id = uniqueID();
+  let name = document.querySelector(".forum__input-name-box").value;
+  let date = new Date().toLocaleDateString();
+  let comment = document.querySelector(".forum__input-comment-box").value;
+  let newComment = { id: id, name: name, date: date, comment: comment };
+
+  if (name !== "" && comment !== "") {
+    comments.unshift(newComment);
+    render();
+    name = document.querySelector(".forum__input-name-box").value = "";
+    comment = document.querySelector(".forum__input-comment-box").value = "";
+  } else {
+    alert("Both input fields are required");
+  }
+});
